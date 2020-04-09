71a49c1c4ef55854c5e57e6953007a7e60147faf
JY <johnathanlee72@hotmail.com>
2020-04-08 23:51:22 -0600

    added locations router and service with a get endpoint test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,9 @@
 const app = require('../src/app');
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / responds with 200', () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
+      .get('/foodles')
+      .expect(200, 'Hello, team Foodles!')
   });
 });
