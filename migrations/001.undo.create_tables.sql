6f8b2b65fcb728e47e6b7311a32e414dc825420b
Zachary <zachary.zboncak@gmail.com>
2020-04-08 23:25:36 -0500

    added operating days tables to migrations

--- migrations/001.undo.create_tables.sql
+++ migrations/001.undo.create_tables.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS operating_days_relations;
+DROP TABLE IF EXISTS operating_days;
 DROP TABLE IF EXISTS tag_relations;
 DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS comments;
---
+++ seeds/seed.operating_days.sql
@@ -0,0 +1,11 @@
+truncate table operating_days CASCADE;
+
+INSERT INTO operating_days (operating_day)
+VALUES
+('Sunday'),
+('Monday'),
+('Tuesday'),
+('Wednesday'),
+('Thursday'),
+('Friday'),
+('Saturday');
