388277f1de7c3d8ea3e764bf4be52678a3661458
Zachary <zachary.zboncak@gmail.com>
2020-04-11 16:00:44 -0500

    server checks if a location is already in the database before it adds a new location

--- src/locations/locationsRouter.js
+++ src/locations/locationsRouter.js
@@ -1,13 +1,13 @@
-const path = require('path')
-const express = require('express')
-const xss = require('xss')
-const LocationService = require('./locationsService')
+const path = require('path');
+const express = require('express');
+const xss = require('xss');
+const LocationService = require('./locationsService');
 const Client = require("@googlemaps/google-maps-services-js").Client; // Google Maps Geocoding API for Node.js. For more info visit https://googlemaps.github.io/google-maps-services-js/
 
 const client = new Client({}); // Instance of the Google Maps Services client.
 
-const locationsRouter = express.Router()
-const jsonParser = express.json()
+const locationsRouter = express.Router();
+const jsonParser = express.json();
 
 const serializeLocation = location => ({
     id: location.id,
@@ -23,7 +23,7 @@ const serializeLocation = location => ({
     website: location.website,
     location_description: xss(location.location_description),
     location_type: location.location_type
-  })
+  });
   
   locationsRouter
     .route('/')
@@ -48,36 +48,41 @@ const serializeLocation = location => ({
             error: { message: `Missing '${key}' in request body` }
           })
       
-      client.geocode({
-        params: {
-          address: `${street_address} ${city}, ${state} ${zip}`,
-          key: process.env.GOOGLE_MAPS_API_KEY
-        }
-      })
-        .then(r => {
-         // console.log(r.data.results[0].geometry.location);
-          const coordinates = r.data.results[0].geometry.location;
-          const location_longitude = coordinates.lng;
-          const location_latitude = coordinates.lat;
-          console.log('location_longitude', location_longitude);
-          console.log('location_latitude', location_latitude);
-          newLocation.location_longitude = location_longitude;
-          newLocation.location_latitude = location_latitude;
-          return LocationService.insertLocation(
-                    req.app.get('db'),
-                    newLocation
-                  )
-                    .then(location => {
-                      res
-                        .status(201)
-                        .location(path.posix.join(req.originalUrl, `/${location.id}`))
-                        .json(serializeLocation(location))
-                    })
-                    .catch(next)
+      LocationService.getByAddress(
+        req.app.get('db'),
+        street_address
+      )
+        .then(location => {
+          if(!location) { // if the location doesn't exist in the database, geocode the address and add it to the database
+            client.geocode({
+              params: {
+                address: `${street_address} ${city}, ${state} ${zip}`,
+                key: process.env.GOOGLE_MAPS_API_KEY
+              }
+            })
+              .then(r => {
+                const coordinates = r.data.results[0].geometry.location;
+                const location_longitude = coordinates.lng;
+                const location_latitude = coordinates.lat;
+                newLocation.location_longitude = location_longitude;
+                newLocation.location_latitude = location_latitude;
+                return LocationService.insertLocation(
+                          req.app.get('db'),
+                          newLocation
+                        )
+                          .then(location => {
+                            res
+                              .status(201)
+                              .location(path.posix.join(req.originalUrl, `/${location.id}`))
+                              .json(serializeLocation(location))
+                          })
+                          .catch(next)
+              })
+              .catch(e => console.log(e));
+          } else { // if the location is already in the database, don't add anything, just return the existing location
+            res.json({existingLocation: location})
+          }
         })
-        .catch(e => console.log(e));
-      
-      
     })
   
     locationsRouter
